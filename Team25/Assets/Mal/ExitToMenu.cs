14815989366671ac17c21973c6ae87d1d716de30
MUKILTEO\1620773 <1620773@mukilteo.wednet.edu>
2023-01-18 13:20:46 -0800

    did a few things

--- Team25/Assets/Mal/ExitToMenu.cs
+++ Team25/Assets/Mal/ExitToMenu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class CreditButton : MonoBehaviour
+public class ExitToMenu : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
