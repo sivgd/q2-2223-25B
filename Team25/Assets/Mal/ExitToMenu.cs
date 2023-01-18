0ab9f331b3b01dab4ca8cd509cd83eea2cc547ec
MUKILTEO\1620773 <1620773@mukilteo.wednet.edu>
2023-01-18 13:01:38 -0800

    did a few things

--- Team25/Assets/Mal/ExitToMenu.cs
+++ Team25/Assets/Mal/ExitToMenu.cs
@@ -8,16 +8,16 @@ public class CreditButton : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     private void OnMouseDown()
     {
-        SceneManager.LoadScene("Credits");
+        SceneManager.LoadScene("Menu");
     }
 }
