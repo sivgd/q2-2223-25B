957461bbd563d21158918724f62799a1280cd86f
MUKILTEO\1620773 <1620773@mukilteo.wednet.edu>
2023-01-20 12:40:20 -0800

    changed the start screen

--- Team25/Assets/Mal/CreditButton.cs
+++ Team25/Assets/Mal/CreditButton.cs
@@ -18,6 +18,6 @@ public class CreditButton : MonoBehaviour
     }
     private void OnMouseDown()
     {
-        SceneManager.LoadScene("Credits");
+        SceneManager.LoadScene("Credits scene");
     }
 }
