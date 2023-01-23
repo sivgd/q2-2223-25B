40d3817393e78ea528a62b9cc27d8ee1d16dfaa6
MUKILTEO\1620773 <1620773@mukilteo.wednet.edu>
2023-01-23 13:30:04 -0800

    added some stuff

---
+++ Team25/Assets/Mal/Credits/CreditRolloer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditRolloer : MonoBehaviour
+{
+    private static int nScreens = 6;
+    private GameObject[] creditScenes = new GameObject[nScreens];
+    private static int swapCount;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //For each credit scene, add a reference here
+        creditScenes[0] = GameObject.find("Credit1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
