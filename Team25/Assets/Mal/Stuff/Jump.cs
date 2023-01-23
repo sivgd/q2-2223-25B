bdc52b9b9c408b28073a80cf20978cea0a9f255d
MUKILTEO\1620628 <1620628@mukilteo.wednet.edu>
2023-01-23 13:19:54 -0800

    Frog Jump animation

--- Team25/Assets/Mal/Stuff/Jump.cs
+++ Team25/Assets/Mal/Stuff/Jump.cs
@@ -9,16 +9,21 @@ public class Jump : MonoBehaviour
     public bool grounded;
     private float currentJumpStrength;
     private Rigidbody2D rb2;
+    Animator a;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2 = GetComponent<Rigidbody2D>();
+        a = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        a.SetFloat("YVelocity", rb2.velocity.y);
+        a.SetBool("Ground", grounded);
+
         if(Input.GetButtonDown("Jump") && grounded == true)
         {
             rb2.AddForce(new Vector2(0, currentJumpStrength));
