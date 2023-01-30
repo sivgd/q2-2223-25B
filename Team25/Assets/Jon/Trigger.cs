a0f4c1f730656b9366830e99163e94645672dfde
MUKILTEO\1620628 <1620628@mukilteo.wednet.edu>
2023-01-30 12:42:38 -0800

    Stopped Herons from getting stuck in walls, prevented more herons from getting stuck in floor

--- Team25/Assets/Jon/Trigger.cs
+++ Team25/Assets/Jon/Trigger.cs
@@ -15,6 +15,7 @@ public class Trigger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        collided = rb2.IsTouchingLayers(0);
+        LayerMask mask = LayerMask.GetMask("Default");
+        collided = rb2.IsTouchingLayers(mask);
     }
 }
