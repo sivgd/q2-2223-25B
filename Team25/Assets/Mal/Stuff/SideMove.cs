7a3f6ceb06a0eda7215f48e14bf2e094a72a37b9
MUKILTEO\1620628 <1620628@mukilteo.wednet.edu>
2023-02-01 12:55:25 -0800

    Added walking audio, merged moving and jumping script

--- Team25/Assets/Mal/Stuff/SideMove.cs
+++ Team25/Assets/Mal/Stuff/SideMove.cs
@@ -6,11 +6,17 @@ public class SideMove : MonoBehaviour
 {
     public float accel;
     public float superaccel;
+    public float jumpStrength;//500
+    public float superJumpStrength;//1000
+    public bool grounded;
     public bool fast;
     private float currentaccel;
+    private float currentJumpStrength;
     private Rigidbody2D rb2;
     private SpriteRenderer sr;
     Animator a;
+    AudioSource steps;
+    private bool isplaying;
 
     // Start is called before the first frame update
     void Start()
@@ -18,8 +24,18 @@ public class SideMove : MonoBehaviour
         rb2 = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
         a = GetComponent<Animator>();
+        steps = GetComponent<AudioSource>();
     }
+    private void Update()
+    {
+        a.SetFloat("YVelocity", rb2.velocity.y);
+        a.SetBool("Ground", grounded);
 
+        if (Input.GetButtonDown("Jump") && grounded == true)
+        {
+            rb2.AddForce(new Vector2(0, currentJumpStrength * transform.localScale.x));
+        }
+    }
     private void FixedUpdate()
     {
 
@@ -28,10 +44,26 @@ public class SideMove : MonoBehaviour
         if(horizValue == 0)
         {
             a.SetBool("Moving", false);
+            if (isplaying == true)
+            {
+                steps.Stop();
+                isplaying = false;
+            }
         }
         else
         {
             a.SetBool("Moving", true);
+            if (isplaying == false && grounded == true)
+            {
+                steps.Play();
+                isplaying = true;
+            }
+
+        }
+        if (grounded == false && isplaying == true)
+        {
+            steps.Stop();
+            isplaying = false;
         }
 
         rb2.AddForce(new Vector2(horizValue * currentaccel, 0));
@@ -49,9 +81,27 @@ public class SideMove : MonoBehaviour
 
 
     }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        grounded = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        grounded = false;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log(collision.tag);
+        if (collision.tag == "SuperJump")
+        {
+            currentJumpStrength = superJumpStrength;
+        }
+        else
+        {
+            currentJumpStrength = jumpStrength;
+        }
         if (fast == true)
         {
             currentaccel = superaccel;
